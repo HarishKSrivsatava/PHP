9fdfea19950b8c603dd0aed0a56240aebb0a2572
Harish Kumar <harishkumar9758@gmail.com>
2021-10-26 19:50:42 +0530

    test

---
+++ data-structure.php
@@ -0,0 +1,8 @@
+<?php
+$str_array = array("Hello", "world");
+
+# var_dump($str_array);
+# var_dump($str_array[1]);
+echo "First element : " . $str_array[0] . "</br>";
+echo "second element : " . $str_array[1];
+?>
