34916ae871bee159f5e9cc47ecb88b47a0706855
Harish Kumar <harishkumar9758@gmail.com>
2021-10-26 19:10:23 +0530

    first commit

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+echo "Hello World !!!";
+echo " This is a sample PHP program";
+$str = "This is a string variable";
+echo $str;
+
+$integer = 5;
+$float = 5.6;
+
+echo "<h1> The integer value : "  . $integer . "<h1>";
+
+
+
+?>
