a8cf2c625d3f0ad0914e1975982fb8aeaa868175
Harish Kumar <harishkumar9758@gmail.com>
2021-10-26 20:38:34 +0530

    formatted

--- control-flow.php
+++ control-flow.php
@@ -2,18 +2,18 @@
 
 $var = 20;
 // If-else block
-if($var < 20){
+if ($var < 20) {
     echo "Inside the if block";
-}else{
+} else {
     echo "Inside the else flow";
 }
 echo "</br>";
 // If-elseif-else block
-if($var < 20){
+if ($var < 20) {
     echo "Inside the if block";
-}elseif($var == 20){
+} elseif ($var == 20) {
     echo "Inside the elseif flow";
-}else{
+} else {
     echo "Inside the else flow";
 }
 
@@ -21,25 +21,28 @@ echo "</br>";
 
 // Check the given number is either divisible by 2 or 5 or both
 $number = 10;
-if($number%2 == 0  or $number%5 == 0 or ($number%2 == 0 and $number%5 == 0)){
+if ($number % 2 == 0  or $number % 5 == 0 or ($number % 2 == 0 and $number % 5 == 0)) {
     echo "Given number " . $number . " is divisible";
-}else{
+} else {
     echo "Given number " . $number . " is not divisible";
 }
 echo "</br>";
 //Switch
 $level = 2;
-switch($level){
-    case 1: echo "This is first level </br>" ;
-            break;
-    case 2: echo "This is second level </br>";
-            break;
-    default : echo "This is default level";
+switch ($level) {
+    case 1:
+        echo "This is first level </br>";
+        break;
+    case 2:
+        echo "This is second level </br>";
+        break;
+    default:
+        echo "This is default level";
 }
 echo "<br>while loop </br>";
 //  While loop
 $while_loop_var = 5;
-while($while_loop_var >= 0){
+while ($while_loop_var >= 0) {
     echo "Variable value is: " . $while_loop_var . "</br>";
     $while_loop_var -= 1;
 }
@@ -47,15 +50,13 @@ while($while_loop_var >= 0){
 echo "<br>do-while loop </br>";
 // do while loop
 $do_while_loop_var = 5;
-do{
+do {
     echo "Variable value is: " . $do_while_loop_var . "</br>";
-    $do_while_loop_var -=1;
-}while($do_while_loop_var >= 0);
+    $do_while_loop_var -= 1;
+} while ($do_while_loop_var >= 0);
 
 # for loop
 echo "<br>for loop </br>";
-for($for_loop_var = 0; $for_loop_var <=3; $for_loop_var++){
+for ($for_loop_var = 0; $for_loop_var <= 3; $for_loop_var++) {
     echo "Value of variable : " . $for_loop_var . "</br>";
 }
-
-?>
