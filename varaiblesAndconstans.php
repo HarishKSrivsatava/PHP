694bc0bfcc79a13d5d138acd2c9d5325375d5a0a
Harish Kumar <harishkumar9758@gmail.com>
2021-10-26 19:48:19 +0530

    Variables and Constants

---
+++ varaiblesAndconstans.php
@@ -0,0 +1,19 @@
+<?php
+echo "Hello World !!!";
+echo " This is a sample PHP program";
+
+#Integer data type
+$str = "This is a string variable";
+echo $str;
+# Integer data type
+$integer = 5;
+#Float data type
+$float = 5.6;
+
+echo "<h3> The integer value : "  . $integer . "<h3>";
+
+
+# Define a constant
+Define("Constant_Name", "This is the constant value");
+echo Constant_Name; // This is the way to access the value of our defined constant.
+?>
